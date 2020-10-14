42ceedb88d3cb6aa8df23dad2ceb9489fe473e77
ashvin <ashvin2266@yahoo.co.in>
2020-10-15 00:43:32 +0100

    Initial set up

--- src/test/java/com/stepDefination/runCukes.java
+++ src/test/java/com/stepDefination/runCukes.java
@@ -4,7 +4,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/resources",tags = "@contactDataTable")
+@CucumberOptions(features = "src/test/java/resources",tags = "@data")
 public class runCukes {
 
 }
